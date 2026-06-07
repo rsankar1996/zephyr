c2f3641490cc83f26aa576c86d6fbd274d02c8f5
Rakesh S <s.rakesh@outlook.in>
2026-06-07 18:00:59 +0530

    corrected the DT_PARENT for overlay value
    Signed-off-by: Rakesh S <s.rakesh@outlook.in>

--- apps/blinky/src/main.c
+++ apps/blinky/src/main.c
@@ -14,10 +14,10 @@
 //Get node identifier for parent led node
 #define LED0_NODE_ID		DT_ALIAS(led0)
 #define LED1_NODE_ID		DT_ALIAS(led1)
-#define LEDS_NODE_ID		DT_PARENT(DT_DRV_INST(LED0_NODE_ID))
+#define LEDS_NODE_ID		DT_PARENT(LED0_NODE_ID)
 
 //Get the blink period from the device tree
-#define DELAY_MS			DT_PROP_OR(LEDS_NODE_ID, blink_period_ms, 5000)
+#define DELAY_MS			DT_PROP_OR(LEDS_NODE_ID, blink_period_ms, 200)
 
 struct gpio_dt_spec  led1 = GPIO_DT_SPEC_GET(LED0_NODE_ID, gpios);
 struct gpio_dt_spec  led2 = GPIO_DT_SPEC_GET(LED1_NODE_ID, gpios);
